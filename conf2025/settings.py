618cc0e2173a66b4fe1afec15207a2a64f1c048c
Aman Bhar <amanbhar.aus@gmail.com>
2025-10-05 14:24:07 +0530

    Update settings.py

--- conf2025/settings.py
+++ conf2025/settings.py
@@ -133,7 +133,7 @@ LOGIN_URL = '/login/'
 
 
 # Email configuration
-EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.console.EmailBackend')
 EMAIL_HOST = config("EMAIL_HOST", default='smtp.gmail.com')
 EMAIL_PORT = config("EMAIL_PORT", default=587, cast=int)
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
